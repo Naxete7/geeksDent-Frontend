f1155102bce56d0dd054b08480f372d03b05668b
Ignacio <naxete7@hotmail.es>
2022-12-21 22:54:02 +0100

    feat: first changes style

---
+++ src/containers/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./Home.scss";
+
+
+const Home=()=>{
+
+
+<h1 className="homeDesign">GeeksDent</h1>
+
+}
+
+
+
+export default Home;
